00343e7683a94d59dfb157ff39f47bb7df675be8
M3DxM3D <127242136+M3DxM3D@users.noreply.github.com>
2023-05-11 12:43:25 +0200

    application architecture

---
+++ src/assets/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+import "./Body.css"
+
+function Body(){
+
+    return (
+
+       <Card/>
+    )
+
+
+}
+
+export default Body
---
+++ src/assets/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Logo.css"
+
+function Logo(){
+
+return 
+
+
+
+}
+
+export default Logo
