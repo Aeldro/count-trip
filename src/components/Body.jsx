2d74538e17c4b369426f635f27b5dccb35faf686
jerome vilanova <jerome.vilanova@gmail.com>
2023-05-12 11:59:31 +0200

    Modif CSS Filtres

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,21 +4,25 @@ import Search from "./Search";
 import "../css/Body.css";
 import Select from "./Select";
 
-
 function Body({ finalData, options, setOptions, text, setText }) {
   return (
     <div className="bodyEl">
-      <div className="FilterContainer">
-      <Search text={text} setText={setText} />
-        <div className="NavFormContainer">
-          <h3 className="h3Nav">Select a continent</h3>
+      <form className="FormContainer">
+        <div className="SelectContainer">
+          <h3 className="h3Filter">Select a continent</h3>
           <Select options={options} setOptions={setOptions} />
         </div>
-      </div>
+        <div className="SearchContainer">
+          <h3 className="h3Filter">Search :</h3>
+          <Search className="SearchBar" text={text} setText={setText} />
+        </div>
+      </form>
       <ul className="cardsList">
         {finalData.length
           ? finalData.map((el) =>
-              (!options && el.name.toLowerCase().includes(text)) || (el.region === options && el.name.toLowerCase().includes(text)) ? (
+              (!options && el.name.toLowerCase().includes(text)) ||
+              (el.region === options &&
+                el.name.toLowerCase().includes(text)) ? (
                 <li className="card" key={el.id}>
                   <Card country={el} />
                 </li>
